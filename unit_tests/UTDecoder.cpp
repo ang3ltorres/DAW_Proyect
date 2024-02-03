49c08e9f09de5c5eccc72403d70848fa8916b452
G4GA <angel.garcia9106@alumnos.udg.mx>
2024-02-02 23:51:18 -0600

    feat: unit_testing target added to project
    in here we add and confgure unit_testing targets for the
    the softwarer components

---
+++ unit_tests/UTDecoder.cpp
@@ -0,0 +1,18 @@
+#include <audioDecoder/include/decoder.hpp>
+#include <projectFile/include/project_file.hpp>
+#include <portaudio.h>
+#include <iostream>
+
+int main() {
+
+	ProjectFileFormat::loadProject("../../project.dawp");
+	PaStreamParameters outputParameters;
+
+	Decoder myDecoder = Decoder();
+
+	myDecoder.changeStr("Hola Mundo");
+
+	std::cout << myDecoder.getStr() << &outputParameters;
+
+	return 0;
+}
