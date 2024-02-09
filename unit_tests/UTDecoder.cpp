3762880c9b3eba2de788c1f2e9cf6087b21d311a
G4GA <Damian.Garcia@toshibagcs.com>
2024-02-09 12:39:30 -0600

    feat: add output formatter for unit tests
    this header is for encapsulating format functions and
    const into one or more namespaces

--- unit_tests/UTDecoder.cpp
+++ unit_tests/UTDecoder.cpp
@@ -1,36 +1,40 @@
-#include <audioDecoder/include/decoder.hpp>
 #include <portaudio.h>
 #include <iostream>
+#include <type_traits>
 
-#define ANSI_RED    "\x1b[31m"
-#define ANSI_GREEN  "\x1b[32m"
-#define ENDC		"\033[0m"
-#define BOLD		"\033[1m"
+#include <format.hpp>
+#include <audioDecoder/include/wav_decoder.hpp>
 
-class DecoderUnitTest {
+class DecoderUnitTest
+{
 	public:
-		void run ();
+		void run();
 	private:
-		Decoder testDecoder;
+		wavDecoder testDecoder;
 		template <class t>
-		bool assertion (const t &result, const t &expected_result);
-		void printResult (bool, const std::string &message);
+		bool assertion(const t &result, const t &expected_result);
+		void printResult(bool, const std::string &message);
 };
 
 template <class t>
-bool DecoderUnitTest::assertion (const t &result, const t &expected_result) {
+bool DecoderUnitTest::assertion(const t &result, const t &expected_result)
+{
 	return result == expected_result;
 }
 
-void DecoderUnitTest::printResult (bool result, const std::string &message) {
-	std::cout << "=> Test ";
+void DecoderUnitTest::printResult (bool result, const std::string &message)
+{
+	std::string result_str = result ? color::output("OK!", color::RED):  color::output("FAILED", color::RED);
+	std::cout << "=> Test [" << color::BOLD << message << color::ENDC << "] " << result_str << " \n";
 }
 
-void DecoderUnitTest::run () {
-
+void DecoderUnitTest::run()
+{
+	// testDecoder.loadFile ("./test_files/sample.wav");
 }
 
-int main() {
+int main()
+{
 	DecoderUnitTest myTester;
 	myTester.run();
 	return 0;
