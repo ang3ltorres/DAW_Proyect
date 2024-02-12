42ee863748f04a0d8788a7e9107c06d4c46f0e9c
G4GA <angel.garcia9106@alumnos.udg.mx>
2024-02-12 08:56:05 -0600

    feat: add some class info methods and contructor declaration
    in here we add some of the class info methods and constructor,
    to start the unit tests without raising exceptions

--- src/audioDecoder/include/wav_decoder.hpp
+++ src/audioDecoder/include/wav_decoder.hpp
@@ -3,6 +3,37 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 class wavDecoder {
+    public:
+        wavDecoder();
+        wavDecoder(const std::string &);
+        //Public methods
+
+        //Loads file and extracts file headers
+        int loadFile(const std::string &);
+        //Information getters
+        //Returns channel count
+        unsigned short getChannelCount() const;
+        //Returns sampleRate in Hz
+        size_t getSampleRate () const;
+        //Returns if is PCM, else, it may have a form of compression that needs
+        //it's way to handle it in the player
+        bool isPCM () const;
+        //Returns a pointer to a dinamycally allocated C type array, that
+        //contains the raw audio Data in PCM format.
+        //receives the channel and returns nullptr if channel if
+        //the channel is larger than the channel count.
+        int *getChannelData (const size_t &) const;
+    private:
+        unsigned short channelCount;
+        size_t sampleRate;
+        unsigned short PCM;
+        std::string filePath;
+        //Im thinking in leave this with a C array, because the pointer
+        //May be to difficult to handle with an std::array
+        std::vector<int *> channelData;
+        int *rawData;
+        // struct otherData {};
 };
--- src/audioDecoder/src/wav_decoder.cpp
+++ src/audioDecoder/src/wav_decoder.cpp
@@ -1,1 +1,31 @@
 #include <../include/wav_decoder.hpp>
+
+wavDecoder::wavDecoder() :
+    channelCount(0),
+    sampleRate(0),
+    PCM(0),
+    filePath(""),
+    rawData(nullptr)
+    {}
+
+int wavDecoder::loadFile(const std::string &filePath)
+{
+    this -> filePath = filePath;
+    // TODO create and call the header decoder
+    return 0;
+}
+
+unsigned short wavDecoder::getChannelCount() const
+{
+    return 0;
+}
+
+bool wavDecoder::isPCM () const
+{
+    return false;
+}
+
+int *wavDecoder::getChannelData (const size_t &channel) const
+{
+    return nullptr;
+}
