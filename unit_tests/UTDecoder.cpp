36a16242fa3ae86f5702b56bf25ea7a617f3f0e1
G4GA <Damian.Garcia@toshibagcs.com>
2024-02-12 12:39:42 -0600

    feat: add unit tests for Decoder class getters and public methods
    we set the pertinent unit tests for the decoder class and leave notes
    for the future changes to be done

--- unit_tests/UTDecoder.cpp
+++ unit_tests/UTDecoder.cpp
@@ -4,18 +4,20 @@
 #include <UT_format.hpp>
 #include <audioDecoder/include/wav_decoder.hpp>
 
+#define TEST_FILEPATH "./test_files/sample2.wav"
+
 class DecoderUnitTest
 {
 	public:
 		void run();
 	private:
 		wavDecoder testDecoder;
-		template <class t>
+		template <typename t>
 		bool assertion(const t &result, const t &expected_result);
 		void printResult(bool, const std::string &message);
 };
 
-template <class t>
+template <typename t>
 bool DecoderUnitTest::assertion(const t &result, const t &expected_result)
 {
 	return result == expected_result;
@@ -23,14 +25,27 @@ bool DecoderUnitTest::assertion(const t &result, const t &expected_result)
 
 void DecoderUnitTest::printResult (bool result, const std::string &message)
 {
-	std::string result_str = result ? color::output("OK!", color::RED):  color::output("FAILED", color::RED);
+	std::string result_str = result ? color::output("OK!", color::RED):  color::output("FAILED!", color::RED);
 	std::cout << "=> Test [" << color::BOLD << message << color::ENDC << "] " << result_str << " \n";
 }
 
 void DecoderUnitTest::run()
 {
-	std::cout << color::output("Testing .wav info extraction", color::BOLD) << "\n";
-	testDecoder.loadFile ("./test_files/sample.wav");
+	bool result = false;
+	std::cout << color::output("Testing file header info", color::BOLD) << "---------\n";
+	testDecoder.loadFile (TEST_FILEPATH);
+
+	printResult(assertion(testDecoder.PCM_value(), (unsigned int)1),
+				"Getting PCM value");
+
+	printResult(assertion(testDecoder.getSampleRate(), (size_t) 44100),
+		        "Getting sample rate");
+
+	printResult(assertion(testDecoder.getChannelCount(), (unsigned short)2),
+				"Getting channel count");
+
+	std::cout << color::output("Testing file raw data", color::BOLD) << "---------\n";
+	//TODO: create bin file with test raw data from ex_sample file
 }
 
 int main()
